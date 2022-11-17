fb90659c268ce7b3f2a45343855bc0c3ffd273f0
Stanislav Barsukov <min.stanis@yandex.ru>
2022-11-17 15:34:00 +0100

    new

--- frontend/src/components/CheckRoute/CheckRoute.js
+++ frontend/src/components/CheckRoute/CheckRoute.js
@@ -3,8 +3,7 @@ import { Navigate } from 'react-router-dom';
 const CheckRoute = ({ children }) => {
   const token = localStorage.getItem('token')
   if (token) {
-    console.log(!token)
-    return <Navigate to="/" />;
+    return <Navigate to="/" replace />;
   }
   return children;
 };
--- frontend/src/components/ProtectedRoute/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 
 const ProtectedRoute = ({ children, loggedIn }) => {
   const location = useLocation().pathname;
-  return loggedIn ? children : <Navigate to={location} />;
+  return loggedIn ? children : <Navigate to={location} replace />;
 };
 
 export default ProtectedRoute;
